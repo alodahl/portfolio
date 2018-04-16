fc37f10a33e563f19d582b22fd05306b0fdd1e3c
Alina L <alodahl@pointloma.edu>
2018-04-15 17:12:15 -0700

    add toggling dropdown nav menu for small widths

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+if ($(window).width() > 529) {
+   $('.js-navlinks-only').removeClass("hidden");
+}
+
+$('.navlinks-div').on('click', '.dropdown-icon', function() {
+  if ($('.js-navlinks-only').hasClass("hidden")) {
+    $('.js-navlinks-only').removeClass("hidden");
+  } else {
+    $('.js-navlinks-only').addClass("hidden");
+  }
+})
