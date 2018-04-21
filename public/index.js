98943bcac86e2b06505acc9f772a2624e45a2aa9
Alina L <alodahl@pointloma.edu>
2018-04-21 16:04:36 -0700

    open new window for links, a11y compatibility

--- public/index.js
+++ public/index.js
@@ -7,7 +7,10 @@ if ($(window).width() > 529) {
 $('.navlinks-div').on('click', '.dropdown-icon', function() {
   if ($('.js-navlinks-only').hasClass("hidden")) {
     $('.js-navlinks-only').removeClass("hidden");
+    $('.dropdown-icon').prop('aria-pressed', true);
+    document.getElementById("js-navlinks-only").focus();
   } else {
     $('.js-navlinks-only').addClass("hidden");
+    $('.dropdown-icon').prop('aria-pressed', false);
   }
 })
