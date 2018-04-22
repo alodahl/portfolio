f729fef68a19d9107416a85091c400e145cf491d
Alina L <alodahl@pointloma.edu>
2018-04-21 19:02:27 -0700

    add README, remove commenting, add screenshot to meta

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio
+
+This professional portfolio showcases my web development and design skills, and includes links to view my artwork, github, and linkedIn profiles as well.
+
+[Check out alinalodahl.com](http://alinalodahl.com/)
+
+## Technologies Used
+
+My portfolio site mainly employs HTML, CSS, JavaScript, jQuery, and a NodeJS backend. It is deployed with Heroku and sends secure messages via SendGrid's mail API.
+
+![Screen Shots](./images/screenshot-large.jpg)
--- app.js
+++ app.js
@@ -9,12 +9,9 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,20 +22,17 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use('/contact', index);
 app.use('/', index);
 console.log("Your app is listening at PORT 3000");
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
